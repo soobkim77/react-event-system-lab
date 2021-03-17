20a67cf52fcf9b3bb07a926583b72b5fd6f6e5fd
Soo-Beom <soobeomkim77@gmail.com>
2021-03-17 10:56:57 -0700

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+    handleOnFocus = (event) => {console.log('Good!')}
+    handleOnBlur = (event) => {console.log('Hey! Eyes on me!')}
+    render(){
+        return <button onFocus={this.handleOnFocus} onBlur={this.handleOnBlur}></button>
+    }
+}
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class KeyPad extends React.Component{
+
+    handleKeyUp = (event) => {console.log("Entering password...")}
+    render(){
+        return <input onKeyUp={this.handleKeyUp} type="password"></input>
+    }
+}
+
+
+export default KeyPad
